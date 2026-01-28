f24d2a629f660ddc409c244d45a3f3433179c0b4
D1ff1cult42 <orbitovkek@gmail.com>
2026-01-28 20:54:24 +0300

    :sparkles: initial menu-service

---
+++ menu-service/src/main/java/org/d1ff/menuservice/MenuServiceApplication.java
@@ -0,0 +1,13 @@
+package org.d1ff.menuservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MenuServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MenuServiceApplication.class, args);
+    }
+
+}
