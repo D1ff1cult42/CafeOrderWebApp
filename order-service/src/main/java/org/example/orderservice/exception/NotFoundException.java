b91440d7cdd700e8ddb9a93f7f9a6d732770bae9
D1ff1cult42 <orbitovkek@gmail.com>
2026-01-28 21:47:40 +0300

    :sparkles: add exceptions

---
+++ order-service/src/main/java/org/example/orderservice/exception/NotFoundException.java
@@ -0,0 +1,10 @@
+package org.example.orderservice.exception;
+
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(org.springframework.http.HttpStatus.NOT_FOUND)
+public class NotFoundException extends RuntimeException {
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
